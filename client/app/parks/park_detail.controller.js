301590996827c8c851ddea88d140b366d2856679
wykhuh <wykhuh@users.noreply.github.com>
2015-03-01 07:55:02 -0800

    show map with marker for park

--- client/app/parks/park_detail.controller.js
+++ client/app/parks/park_detail.controller.js
@@ -1,11 +1,24 @@
 'use strict';
 
 angular.module('parksandrecApp')
-  .controller('ParkDetailCtrl', function ($scope, parksService, $stateParams) {
+  .controller('ParkDetailCtrl', ['$scope', 'parksService', '$stateParams', 'uiGmapGoogleMapApi',
+    function ($scope, parksService, $stateParams, uiGmapGoogleMapApi) {
 
     parksService.getParkDetail($stateParams.id).then(function(park){
+
+      // uiGmapGoogleMapApi is a promise.
+      // The "then" callback function provides the google.maps object.
+      uiGmapGoogleMapApi.then(function(maps) {
+        $scope.map = { center: { latitude: park.data.lat, longitude: park.data.long }, zoom: 11 };
+      });
+
       $scope.park = park.data;
       $scope.features = park.data.features.join(', ');
+
+      $scope.parkMarkers = [{latitude: park.data.lat, longitude: park.data.long, title: 'me', id: 1}];
+
     });
 
-  });
+  }]);
+
+
