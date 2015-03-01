ab7d446a921f9a86d5c76c8a7c79a0acd49c32f1
alexzkazu <alex.ziebol@gmail.com>
2015-03-01 13:00:22 -0800

    angularized html

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -5,7 +5,7 @@ angular.module('parksandrecApp')
     $stateProvider
       .state('main', {
         url: '/',
-        templateUrl: 'app/main/main.html',
+        templateUrl: 'app/views/initial.html',
         controller: 'MainCtrl'
       });
   });
