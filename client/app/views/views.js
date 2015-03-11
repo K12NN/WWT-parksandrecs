72ec2854ff7c1fc630404ae591bf56e9fa6f5cce
alexzkazu <alex.ziebol@gmail.com>
2015-03-10 19:00:24 -0700

    refactored code, needs more

--- client/app/views/views.js
+++ client/app/views/views.js
@@ -11,67 +11,61 @@ angular.module('parksandrecApp')
 
       .state('where', {
         url: '/where',
-        templateUrl: 'app/views/where.html',
+        templateUrl: 'app/views/topics.html',
         controller: 'WhereCtrl'
       })
 
       .state('what', {
         url: '/what',
-        templateUrl: 'app/views/what.html',
+        templateUrl: 'app/views/topics.html',
         controller: 'WhatCtrl'
-      })
-
-      .state('land', {
-        url: '/land',
-        templateUrl: 'app/views/land.html',
-        controller: 'LandCtrl'
       })
 
       .state('water', {
         url: '/water',
         templateUrl: 'app/views/water.html',
         controller: 'WaterCtrl'
-      })
-
-      .state('beaches', {
-        url: '/beaches',
-        templateUrl: 'app/views/beaches.html',
-        controller: 'BeachCtrl'
-      })
-
-      .state('lakes', {
-        url: '/lakes',
-        templateUrl: 'app/views/lakes.html',
-        controller: 'LakeCtrl'
-      })
-
-      .state('pools', {
-        url: '/pools',
-        templateUrl: 'app/views/pools.html',
-        controller: 'PoolCtrl'
-      })
-
-      .state('fitness', {
-        url: '/fitness',
-        templateUrl: 'app/views/fitness.html',
-        controller: 'FitCtrl'
-      })
-
-      .state('cultural', {
-        url: '/cultural',
-        templateUrl: 'app/views/cultural.html',
-        controller: 'CultureCtrl'
-      })
-
-      .state('scenic', {
-        url: '/scenic',
-        templateUrl: 'app/views/scenic.html',
-        controller: 'SceneCtrl'
-      })
-      
-      .state('sports', {
-        url: '/sports',
-        templateUrl: 'app/views/sports.html',
-        controller: 'SportCtrl'
       });
+
+      // .state('beaches', {
+      //   url: '/beaches',
+      //   templateUrl: 'app/views/beaches.html',
+      //   controller: 'BeachCtrl'
+      // })
+
+      // .state('lakes', {
+      //   url: '/lakes',
+      //   templateUrl: 'app/views/lakes.html',
+      //   controller: 'LakeCtrl'
+      // })
+
+      // .state('pools', {
+      //   url: '/pools',
+      //   templateUrl: 'app/views/pools.html',
+      //   controller: 'PoolCtrl'
+      // })
+
+      // .state('fitness', {
+      //   url: '/fitness',
+      //   templateUrl: 'app/views/fitness.html',
+      //   controller: 'FitCtrl'
+      // })
+
+      // .state('cultural', {
+      //   url: '/cultural',
+      //   templateUrl: 'app/views/cultural.html',
+      //   controller: 'CultureCtrl'
+      // })
+
+      // .state('scenic', {
+      //   url: '/scenic',
+      //   templateUrl: 'app/views/scenic.html',
+      //   controller: 'SceneCtrl'
+      // })
+
+      // .state('sports', {
+      //   url: '/sports',
+      //   templateUrl: 'app/views/sports.html',
+      //   controller: 'SportCtrl'
+      // });
   });
